3700dc4f34585eba540ad86d9118c215c76a6382
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-07-22 15:55:08 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+// code your solution here //
+function saturdayFun(what= "roller-skate"){
+    return `This Saturday, I want to ${what}!`
+}
+function mondayWork(where = "go to the office"){
+    return `This Monday, I will ${where}.`
+}
+function wrapAdjective(flair = '*') {
+    return function (adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
